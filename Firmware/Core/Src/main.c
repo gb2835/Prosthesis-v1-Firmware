544622012609207dfc95db1a50f0a51adf11e737
Hub-Robotics <161149676+Hub-Robotics@users.noreply.github.com>
2024-03-25 18:03:45 -0500

    Removed mpu-9255.h

--- Firmware/Core/Src/main.c
+++ Firmware/Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "mpu-9255.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
