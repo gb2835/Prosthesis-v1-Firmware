5eb6df703f794b1c2170b4240201e4240dfb33b2
Hub-Robotics <161149676+Hub-Robotics@users.noreply.github.com>
2024-05-31 17:11:14 -0500

    Add files via upload

---
+++ prosthesis_control.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ *
+ * TITLE   Prosthesis Control
+ * AUTHOR  Greg Berkeley
+ * RELEASE XX/XX/XXXX
+ *
+ * NOTES
+ * 1. See source file for more information.
+ *
+ ******************************************************************************/
+
+#ifndef INC_PROSTHESIS_CONTROL_H_
+#define INC_PROSTHESIS_CONTROL_H_
+
+
+/*******************************************************************************
+ * PUBLIC DEFINITIONS
+ ******************************************************************************/
+
+enum TestPrograms_e
+{
+	None,				// No test program runs
+	ReadOnly,
+	ConstantTorque,
+	MagneticEncoderBias,
+	ImpedanceControl
+};
+
+extern uint16_t CAN_ID;
+extern uint8_t isProsthesisControlRequired;
+
+void RunProsthesisControl (void);
+void RequireTestProgram ( enum TestPrograms_e option );
+
+
+/*******************************************************************************
+ * END
+ ******************************************************************************/
+
+#endif /* INC_PROSTHESIS_CONTROL_H_ */
