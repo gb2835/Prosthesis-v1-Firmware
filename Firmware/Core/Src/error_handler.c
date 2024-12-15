5ba79da2ddd92ce1bd0938352e93b6fa74c767d1
Hub-Robotics <gberkeley@crimson.ua.edu>
2024-12-15 14:48:02 -0600

    General cleanup

--- Firmware/Core/Src/error_handler.c
+++ Firmware/Core/Src/error_handler.c
@@ -19,17 +19,17 @@
 typedef enum
 {
 	NoError,
-	Ankle_AS5145B_InitError,
-	Ankle_EPOS4_InitError,
-	Ankle_EPOS4_FaultError,
-	Ankle_EPOS4_AbortError,
-	Ankle_MCP25625_InitError,
-	Knee_AS5145B_InitError,
-	Knee_EPOS4_InitError,
-	Knee_EPOS4_FaultError,
-	Knee_EPOS4_AbortError,
-	Knee_MCP25625_InitError,
-	MPU925x_InitError
+	AnkleEncoderInitError,
+	AnkleMotorControllerInitError,
+	AnkleMotorControllerFaultError,
+	AnkleMotorControllerAbortError,
+	AnkleCAN_ControllerInitError,
+	KneeEncoderInitError,
+	KneeMotorControllerInitError,
+	KneeMotorControllerFaultError,
+	KneeMotorControllerAbortError,
+	KneeCAN_ControllerInitError,
+	IMU_InitError
 } LED_Code_e;
 
 static LED_Code_e CM_ledCode = NoError;
@@ -37,9 +37,9 @@ static LED_Code_e CM_ledCode = NoError;
 void ErrorHandler_AS5145B(EncoderIndex_e deviceIndex, AS5145B_Error_e error)
 {
 	if(deviceIndex == AnkleEncoderIndex)
-		CM_ledCode = Ankle_AS5145B_InitError;
+		CM_ledCode = AnkleEncoderInitError;
 	else
-		CM_ledCode = Knee_AS5145B_InitError;
+		CM_ledCode = KneeEncoderInitError;
 
 	while(1);
 }
@@ -49,20 +49,20 @@ void ErrorHandler_EPOS4(MotorControllerIndex_e deviceIndex, EPOS4_Error_e error)
 	if(deviceIndex == AnkleMotorControllerIndex)
 	{
 		if(error == EPOS4_FaultError)
-			CM_ledCode = Ankle_EPOS4_FaultError;
+			CM_ledCode = AnkleMotorControllerFaultError;
 		else if(error == EPOS4_AbortError)
-			CM_ledCode = Ankle_EPOS4_AbortError;
+			CM_ledCode = AnkleMotorControllerAbortError;
 		else
-			CM_ledCode = Ankle_EPOS4_InitError;
+			CM_ledCode = AnkleMotorControllerInitError;
 	}
 	else
 	{
 		if(error == EPOS4_FaultError)
-			CM_ledCode = Knee_EPOS4_FaultError;
+			CM_ledCode = KneeMotorControllerFaultError;
 		else if(error == EPOS4_AbortError)
-			CM_ledCode = Knee_EPOS4_AbortError;
+			CM_ledCode = KneeMotorControllerAbortError;
 		else
-			CM_ledCode = Knee_EPOS4_InitError;
+			CM_ledCode = KneeMotorControllerInitError;
 	}
 
 	while(1);
@@ -71,16 +71,16 @@ void ErrorHandler_EPOS4(MotorControllerIndex_e deviceIndex, EPOS4_Error_e error)
 void ErrorHandler_MCP25625(uint8_t deviceIndex, MCP25625_Error_e error)
 {
 	if(deviceIndex == AnkleCAN_ControllerIndex)
-		CM_ledCode = Ankle_MCP25625_InitError;
+		CM_ledCode = AnkleCAN_ControllerInitError;
 	else
-		CM_ledCode = Knee_MCP25625_InitError;
+		CM_ledCode = KneeCAN_ControllerInitError;
 
 	while(1);
 }
 
 void ErrorHandler_MPU925x(MPU925x_Error_e error)
 {
-	CM_ledCode = MPU925x_InitError;
+	CM_ledCode = IMU_InitError;
 	while(1);
 }
 
